3cb4fe974194ca2684b82ed288fee5e8692b3462
chris <5sensprod@gmail.com>
2023-05-12 06:37:59 +0200

    correction indentation

--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -27,35 +27,35 @@ export default class {
     $('#modaleFile').modal('show')
   }
 
-getBills = () => {
-  if (this.store) {
-    return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-          .map(doc => {
-            try {
-              return {
-                ...doc,
-                // utilisation de la date formatée
-                formattedDate: formatDate(doc.date),
-                status: formatStatus(doc.status)
+  getBills = () => {
+    if (this.store) {
+      return this.store
+        .bills()
+        .list()
+        .then(snapshot => {
+          const bills = snapshot
+            .map(doc => {
+              try {
+                return {
+                  ...doc,
+                  // utilisation de la date formatée
+                  formattedDate: formatDate(doc.date),
+                  status: formatStatus(doc.status)
+                }
+              } catch (e) {
+                console.log(e, 'for', doc)
+                return {
+                  ...doc,
+                  date: doc.date,
+                  status: formatStatus(doc.status)
+                }
               }
-            } catch(e) {
-              console.log(e,'for',doc)
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status)
-              }
-            }
-          })
-          // tri des factures par date décroissante
-          .sort((a, b) => new Date(b.date) - new Date(a.date));
-        console.log('length', bills.length)
-        return bills
-      })
+            })
+            // tri des factures par date décroissante
+            .sort((a, b) => new Date(b.date) - new Date(a.date));
+          console.log('length', bills.length)
+          return bills
+        })
+    }
   }
-}
 }
